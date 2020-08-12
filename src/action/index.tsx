2bb608871aa6709f916728e3825bb524f4dfba0d
prjunkey <prjunkey@gmail.com>
2020-08-12 20:30:04 +0900

    finished

---
+++ src/action/index.tsx
@@ -0,0 +1,5 @@
+export const SET_POPULAR = "SET_POPULAR";
+export const SET_RELATED = "SET_RELATED";
+export const SET_SELECTED = "SET_SELECTED";
+export const SET_TERM = "SET_TERM";
+export const SET_SEARCHED = "SET_SEARCHED";
